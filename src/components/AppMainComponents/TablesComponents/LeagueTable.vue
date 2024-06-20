4012f8b0303530633ca43fd86c2068d9361fa720
martinaliporata <martinaliporata@gmail.com>
2024-06-20 17:37:39 +0200

    implementing league table

--- src/components/AppMainComponents/TablesComponents/LeagueTable.vue
+++ src/components/AppMainComponents/TablesComponents/LeagueTable.vue
@@ -1,14 +1,65 @@
 <script>
+import {store} from '../../../store'
 export default {
 data() {
-return {  
-}
-}}
+    return {  
+        store
+        }
+    }}
 </script>
 
 <template>
-    <h1>ciao da league table</h1>
+    <h1>League table</h1>
+    <table>
+        <thead>
+            <tr>
+                <th class="w-40">team</th>
+                <th>W</th>
+                <th>D</th>
+                <th>L</th>
+                <th>PTS</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(team, index) in store.teams" :key="team.id">
+                <td class="largest-column">
+                    {{ team.id }}
+                    <img :src="team.logo" :alt="team.name" class="team-logo">
+                    {{ team.name }}
+                </td>
+                <td>
+                    {{ team.win }}
+                </td>
+                <td>
+                    {{ team.draw }}
+                </td>
+                <td>
+                    {{ team.lost }}
+                </td>
+                <td>
+                    {{ team.points }}
+                </td>
+            </tr>
+        </tbody>
+        <tfoot class="button">
+            <tr>
+                <td>
+                    <button> View full league table</button>
+                </td>
+            </tr>
+        </tfoot>
+    </table>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@use '../node_modules/bootstrap/scss/bootstrap.scss';
+    h1{
+        font-weight: bold;
+        color: black;
+    }
+    
+    .largest-column {
+        width: 50%;
+    }
+
 </style>
