f55773840bf9b95de5f326f7d96b350dc42ec2c0
fedtoscano <federicotoscano611@gmail.com>
2024-06-20 12:49:19 +0200

    aggiornato scaffolding con nuovi componenti

--- src/components/AppMainComponents/MainLeagueTables.vue
+++ src/components/AppMainComponents/MainLeagueTables.vue
@@ -1,12 +1,13 @@
 <script>
 import { store } from '../../store.js';
+import LeagueTable from './TablesComponents/LeagueTable.vue';
+import FixtureTables from "./TablesComponents/FixtureTables.vue"
 
 
 export default {
     components:{
-       
-        
-
+        LeagueTable,
+        FixtureTables
     },
 data() {
 return {  
@@ -16,7 +17,8 @@ return {
 </script>
 
 <template>
-  
+    <LeagueTable/>
+    <FixtureTables/>
 </template>
 
 <style>
---
+++ src/components/AppMainComponents/TablesComponents/LeagueTable.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+data() {
+return {  
+}
+}}
+</script>
+
+<template>
+    <h1>ciao da league table</h1>
+</template>
+
+<style scoped>
+</style>
